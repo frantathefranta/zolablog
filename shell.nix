d9193ba457a7038630170397bd7047f1e38fac04
Franta Bartik <fb@franta.us>
2026-02-27 15:58:06 -0500

    Tweaked shell.nix

--- shell.nix
+++ shell.nix
@@ -1,12 +1,6 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-let
-  radion-theme = builtins.fetchTarball {
-    name = "radion";
-    url = "https://github.com/frantathefranta/radion/archive/063cc74.tar.gz";
-    sha256 = "1ci71qfpmwn1hzgx75qsdmnykhabkr3lrv9rrpam0pq6q00bgl8m";
-  };
-in
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 with pkgs;
 
@@ -16,7 +10,6 @@ mkShell {
   ];
 
   shellHook = ''
-    mkdir -p themes
-    ln -snf "${radion-theme}" themes/radion
+    ${pkgs.git} submodule update --init --recursive
   '';
 }
