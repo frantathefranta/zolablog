e47ba69320124e60c7b9bc90e7fc5fce93790b1d
Franta Bartik <fb@franta.us>
2026-03-02 16:53:53 -0500

    Added renovate datasource comment to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 main() {
+    # renovate: datasource=github-releases depName=getzola/zola
     ZOLA_VERSION=0.22.1
 
     curl -sLJO "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
@@ -10,6 +11,6 @@ main() {
     ./zola build
 }
 
-main
+main 
 
 set -euo pipefail
